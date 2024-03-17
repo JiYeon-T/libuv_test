d856073b3563c3d478f61f8f027bef27cf6b36a4
qz <qizhen_nxu@163.com>
2024-03-17 08:10:33 +0800

    uv_idle test
    Signed-off-by: qz <qizhen_nxu@163.com>

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
-# libuv_test
+##### 1. libuv
+基于事件循环的异步 IO 支持;
+没有任何事件时, 就会退出 v_run()
+特定的事件类型:计时器, IO设备, TCP/UDP, 异步事件等等
 
 
-compile:
+##### 2. compile:
+mkdir build && cd build
+cmake ..
+make
+
 cmake -G "Unix Makefiles" -B .
 make
--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "../external/libuv/include/uv.h"
+#include "uv.h"
 
 
 #define FIXTURE "testfile"
@@ -10,6 +10,7 @@ static uv_fs_poll_t poll_handle;
 static uv_timer_t timer_handle;
 static uv_timer_t timer_handle2;
 static int timer_called = 0;
+static int idle_cnt = 0;
 
 static void timer_cb(uv_timer_t* handle) {
 //   ASSERT_PTR_EQ(handle, &timer_handle);
@@ -31,12 +32,21 @@ static void poll_cb_fail(uv_fs_poll_t* handle,
     printf("fail_cb called status:%d\n", status);
 }
 
+static void idle_cb(uv_idle_t *handle)
+{
+    ++idle_cnt;
+    printf("idle:%d\n", idle_cnt);
+    if (idle_cnt > 5) {
+        printf("stop idle\n");
+        uv_idle_stop(handle);
+    }
+}
+
 int main(int argc ,char *argv[])
 {
-
-
     printf("lib uv version:%s\n", uv_version_string());
     uv_loop_t *loop = malloc(sizeof(uv_loop_t));
+    uv_idle_t idler;
     uv_loop_init(loop);
 
     uv_timer_init(loop, &timer_handle);
@@ -49,6 +59,8 @@ int main(int argc ,char *argv[])
 
     uv_fs_poll_start(&poll_handle, poll_cb_fail, FIXTURE, 1000);
 
+    uv_idle_init(loop, &idler);
+    uv_idle_start(&idler, idle_cb);
 
     printf("Now quitting.\n");
     uv_run(loop, UV_RUN_DEFAULT);
@@ -57,5 +69,6 @@ int main(int argc ,char *argv[])
     uv_loop_close(loop);
     printf("alive:%d\n", uv_loop_alive(loop));
     free(loop);
+
     return 0;
 }
